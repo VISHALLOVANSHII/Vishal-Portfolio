330eb078f27daa57a31cb57623e65dbf7af262c7
Vishal Lovanshi <vishallovanshi6@gmail.com>
2026-07-05 01:52:26 +0530

    Add GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/Vishal-Portfolio/",
   plugins: [react(), tailwindcss()],
 });
